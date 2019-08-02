1afb8b5157a54552180d5922b68bb2689b5a869c
Yan_Pustynnyy <necrosith@yandex.ru>
2019-08-02 23:32:48 +0300

    Sliders added to wecando block

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('.projects__slider').slick({
+        // dots: true,
+        // infinite: true,
+        // speed: 300,
+        centerMode: false,
+        slidesToShow: 3
+    });
+
+    $('.wecando__slider-one').slick({
+        // dots: true,
+        // infinite: true,
+        // speed: 300,
+        centerMode: false,
+        slidesToShow: 3
+    });
+
+    $('.wecando__slider-two').slick({
+        // dots: true,
+        // infinite: true,
+        // speed: 300,
+        centerMode: false,
+        slidesToShow: 3
+    });
+});
